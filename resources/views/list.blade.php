88776de346bc5a7463735ce54be6c8a58de8108c
xgerhard <xgerhard@2g.be>
2021-02-04 23:26:02 +0100

    Update view info to use queue position to remove/promote

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -21,6 +21,13 @@
                 </div>
             </div>
 
+            <div class="alert alert-info alert-dismissible" role="alert">
+                <strong>Info for moderators:</strong> you might have noticed the Remove / Promote ID is gone from this page, instead you can now use the queue position of the user to remove or promote the user. The usage will be the same.
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
             <div class="row">
                 @if(isset($channel->queues) && !empty($channel->queues))
                 @foreach($channel->queues as $queue)
@@ -35,7 +42,7 @@
                             <ul class="list-group">
                                 @if(isset($queue->queueUsers) && !empty($queue->queueUsers))
                                 @foreach($queue->queueUsers as $i => $user)
-                                <li class="list-group-item">{{ $i+1 }}. {{ $user->user->displayName }}<span class="badge badge-primary badge-pill">Remove / Promote ID: {{ $user->id }}</span>
+                                <li class="list-group-item">{{ $i+1 }}. {{ $user->user->displayName }}
                                     @if(trim($user->message) != "")<blockquote class="user-message">{{ $user->message }}</blockquote>@endif
                                 </li>
                                 @endforeach
