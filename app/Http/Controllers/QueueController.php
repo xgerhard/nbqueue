08fbedb0ce70598cd26de1808f55862185fdebf3
xgerhard <xgerhard@2g.be>
2021-02-04 23:10:21 +0100

    Add update instructions

--- app/Http/Controllers/QueueController.php
+++ app/Http/Controllers/QueueController.php
@@ -12,6 +12,7 @@ class QueueController extends BaseController
         if($channelId && is_int($channelId))
         {
             $oChannel = Channel::with([
+                'channelOwner',
                 'queues' => function ($q) {
                     $q->orderBy('created_at', 'ASC');
                 },
@@ -22,20 +23,11 @@ class QueueController extends BaseController
             ])
             ->findOrFail($channelId);
 
-            return view('list', ['channel' => $oChannel]);
-
-            /* Usage
-            $oChannel->channelOwner()->displayName;
-            foreach($oChannel->queues as $oQueue)
-            {
-                echo $oQueue->name .'<br/><ul>';
-                foreach($oQueue->queueUsers as $oQueueUser)
-                {
-                    echo '<li>'. $oQueueUser->user->displayName .'</li>';
-                }
-                echo '</ul>';
-            }*/
+            if(!$oChannel->channelOwner)
+                return 'Woops, this page has been updated. <br/><br/>Please run the !q command once in your channel (or the channel you are watching), for example "!q list". <br/><br/>Refresh this page after, if this error is not resolved, please contact xgerhard on Twitch or @gerhardoh on Twitter. <br/><br/>Sorry for the inconvenience :)';
+            else
+                return view('list', ['channel' => $oChannel]);
         }
-        else echo 'invalid channelId';
+        else return 'Invalid channelId';
     }
 }
