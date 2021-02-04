144978db332fc4a70c859cc415341c1982094892
xgerhard <xgerhard@2g.be>
2021-02-04 23:28:42 +0100

    Cleanup routes

--- routes/web.php
+++ routes/web.php
@@ -27,33 +27,3 @@ $router->get('/', 'CommandController@QueryParser');
 $strListPath = explode('.', $_SERVER['HTTP_HOST'])[0] === 'nbq' ? '' : 'list';
 
 $router->get($strListPath .'/{channelId}[/{name}]', 'QueueController@list');
-
-/*$router->get('/', function () use ($router) {
-    return $router->app->version();
-});*
-
-
-/*
-Server commands
-----
-!setq $name // Switches queue channel
-!currq // Display current queue name and list info
-
-
-!next (!q next) $number // Pick next $number users from queue
-!random (!q random) // Pick random user from queue
-!open (!q open) // Open queue
-!close (!q close) // Close queue
-!add (!q add) $user "message" // Adds user to queue
-!remove (!q remove) $user // Removes user from queue
-
-// Settings
-!q setsize 15
-!q 
-
-User commands
----
-
-!join (!q join) "optional message"
-!q leave
-*/
